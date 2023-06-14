87fb88f64838425b210674d874174ed336a61481
tirthkparikh <tirthkparikh@gmail.com>
2023-06-14 10:14:31 +0530

    fourth commit

--- README.md
+++ README.md
@@ -14,57 +14,17 @@ Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
 The page will reload when you make changes.\
 You may also see any lint errors in the console.
 
-### `npm test`
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Finding Falcone
+Our problem is set in the planet of Lengaburu...in the distant distant galaxy of Tara B. After the recent war with neighbouring planet Falicornia, King Shan has exiled the Queen of Falicornia for 15 years.
+Queen Al Falcone is now in hiding. But if King Shan can find her before the years are up, she will be exiled for another 15 years....
 
-### `npm run build`
+### src public
+src folder contanins - css and js(react) file
+public-html
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### react-router-dom
+react router version 5.3.4 is used for routing through components
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+#node version 
+V18.16.0
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "../components/FindFalcon.css";
-import Table from './Table';
-import { useHistory } from 'react-router-dom';
+import Table from "./Table";
+import { useHistory } from "react-router-dom";
 
 export default function Form(props) {
   const [data, setData] = useState([""]);
   const [vehicle, setvehicle] = useState([]);
   const [planet, setplanet] = useState([]);
   const [vehicleQty, setVehicleQty] = useState(new Map());
-  const[vehicledist, setvehicleDist]= useState(new Map())
-  const[vehiclemax, setvehicleMax]= useState(new Map())
+  const [vehicledist, setvehicleDist] = useState(new Map());
+  const [vehiclemax, setvehicleMax] = useState(new Map());
   const [selection1, setSelection1] = useState("");
   const [selection2, setSelection2] = useState("");
   const [selection3, setSelection3] = useState("");
@@ -20,9 +20,9 @@ export default function Form(props) {
   const [selectedOption4, setSelectedOption4] = useState("");
   const [prevVehicle, setprevvehicle] = useState(new Map());
   const [planetdis, setplanetdis] = useState(new Object());
-  const[time,Settime]=useState(new Object())
-  
-  let responseData=''
+  const [time, Settime] = useState(new Object());
+
+  let responseData = "";
   const history = useHistory();
   useEffect(() => {
     setData([...props.props]);
@@ -33,10 +33,9 @@ export default function Form(props) {
         );
         const jsonData = await response.json();
         jsonData.forEach((element) => {
-          
           vehicleQty.set(element.name, element.total_no);
-          vehicledist.set(element.name,element.speed)
-          vehiclemax.set(element.name,element.max_distance)
+          vehicledist.set(element.name, element.speed);
+          vehiclemax.set(element.name, element.max_distance);
         });
         setvehicle(jsonData);
         const response1 = await fetch(
@@ -44,52 +43,43 @@ export default function Form(props) {
         );
         const jsonData1 = await response1.json();
         jsonData1.map((item) => {
-         
           planetdis[item.name] = item.distance;
         });
-       
 
         setplanet(jsonData1);
       } catch (error) {
         console.log("Error fetching data:", error);
       }
     };
-    for(let i=1;i<5;i++){
-      time[i]=0
+    for (let i = 1; i < 5; i++) {
+      time[i] = 0;
     }
 
     fetchData();
   }, []);
 
-  
-    
-
- 
-
   const handleVehicleSelection = (e) => {
     //to opreate proper checkbox
     let n = e.target.value.length;
-  
+
     if (e.target.value[n - 1] == 1) {
-      
-      time[1]=(planetdis[selection1]/vehicledist.get(e.target.id))
+      time[1] = planetdis[selection1] / vehicledist.get(e.target.id);
       setSelectedOption1(e.target.id + "1");
-      
     }
 
     if (e.target.value[n - 1] == 2) {
       setSelectedOption2(e.target.id + "2");
-      time[2]=(planetdis[selection2]/vehicledist.get(e.target.id))
+      time[2] = planetdis[selection2] / vehicledist.get(e.target.id);
     }
     if (e.target.value[n - 1] == 3) {
       setSelectedOption3(e.target.id + "3");
-      time[3]=(planetdis[selection3]/vehicledist.get(e.target.id))
+      time[3] = planetdis[selection3] / vehicledist.get(e.target.id);
     }
     if (e.target.value[n - 1] == 4) {
       setSelectedOption4(e.target.id + "4");
-      time[4]=(planetdis[selection4]/vehicledist.get(e.target.id))
+      time[4] = planetdis[selection4] / vehicledist.get(e.target.id);
     }
- 
+
     //to decrese vehicle count
     let count = vehicleQty.get(e.target.id);
     vehicleQty.set(e.target.id, --count);
@@ -98,120 +88,133 @@ export default function Form(props) {
     //to increase vehicle count in case of change of selection
     if (prevVehicle.get(JSON.parse(e.target.name))) {
       let count = vehicleQty.get(prevVehicle.get(JSON.parse(e.target.name)));
-      
 
       vehicleQty.set(prevVehicle.get(JSON.parse(e.target.name)), ++count);
       prevVehicle.set(JSON.parse(e.target.name), e.target.id);
       setVehicleQty(new Map(vehicleQty.entries()));
     } else {
       prevVehicle.set(JSON.parse(e.target.name), e.target.id);
-
-
     }
-  
   };
 
   const handleSelection1Change = (event) => {
     setSelection1(event.target.value);
-    
-    if(selectedOption1){
-    if(vehiclemax.get(selectedOption1.slice(0,-1))<planetdis[event.target.value]){
-      alert("vehicle distance must be more or equal to planet, secnario reset ")
-      window.location.reload()
+
+    if (selectedOption1) {
+      if (
+        vehiclemax.get(selectedOption1.slice(0, -1)) <
+        planetdis[event.target.value]
+      ) {
+        alert(
+          "vehicle distance must be more or equal to planet, secnario reset "
+        );
+        window.location.reload();
+      }
+      time[1] =
+        planetdis[event.target.value] /
+        vehicledist.get(selectedOption1.slice(0, -1));
     }
-    time[1]=(planetdis[event.target.value]/vehicledist.get(selectedOption1.slice(0,-1)))
-    
-  }
-    
   };
 
   const handleSelection2Change = (event) => {
     setSelection2(event.target.value);
-    if(selectedOption2){
-    if(vehiclemax.get(selectedOption2.slice(0,-1))<planetdis[event.target.value]){
-      alert("vehicle distance must be more or equal to planet, secnario reset ")
-      window.location.reload()
+    if (selectedOption2) {
+      if (
+        vehiclemax.get(selectedOption2.slice(0, -1)) <
+        planetdis[event.target.value]
+      ) {
+        alert(
+          "vehicle distance must be more or equal to planet, secnario reset "
+        );
+        window.location.reload();
+      }
+
+      time[2] =
+        planetdis[event.target.value] /
+        vehicledist.get(selectedOption2.slice(0, -1));
     }
-    
-    time[2]=(planetdis[event.target.value]/vehicledist.get(selectedOption2.slice(0,-1)))
-  }
   };
 
   const handleSelection3Change = (event) => {
     setSelection3(event.target.value);
 
-    if(selectedOption3){
-    if(vehiclemax.get(selectedOption3.slice(0,-1))<planetdis[event.target.value]){
-      alert("vehicle distance must be more or equal to planet, secnario reset ")
-      window.location.reload()
+    if (selectedOption3) {
+      if (
+        vehiclemax.get(selectedOption3.slice(0, -1)) <
+        planetdis[event.target.value]
+      ) {
+        alert(
+          "vehicle distance must be more or equal to planet, secnario reset "
+        );
+        window.location.reload();
+      }
+      time[3] =
+        planetdis[event.target.value] /
+        vehicledist.get(selectedOption3.slice(0, -1));
     }
-    time[3]=(planetdis[event.target.value]/vehicledist.get(selectedOption3.slice(0,-1)))
-  }
-    
-    
-
   };
 
   const handleSelection4Change = (event) => {
-  
     setSelection4(event.target.value);
-    if(selectedOption4){
-    if(vehiclemax.get(selectedOption4.slice(0,-1))<planetdis[event.target.value]){
-      alert("vehicle distance must be more or equal to planet, secnario reset ")
-      window.location.reload()
-    }
-    
-    time[4]=(planetdis[event.target.value]/vehicledist.get(selectedOption4.slice(0,-1)))
-  }
-  };
- 
-  const sendPostRequest = async()=>{
-    
-    let finalData= {
-      "token": localStorage.getItem('token'),
-      "planet_names": [selection1,selection2,selection3,selection4],
-      "vehicle_names":[selectedOption1.slice(0,-1),selectedOption2.slice(0,-1),selectedOption3.slice(0,-1),selectedOption4.slice(0,-1)]
+    if (selectedOption4) {
+      if (
+        vehiclemax.get(selectedOption4.slice(0, -1)) <
+        planetdis[event.target.value]
+      ) {
+        alert(
+          "vehicle distance must be more or equal to planet, secnario reset "
+        );
+        window.location.reload();
+      }
 
+      time[4] =
+        planetdis[event.target.value] /
+        vehicledist.get(selectedOption4.slice(0, -1));
     }
-    
+  };
+
+  const sendPostRequest = async () => {
+    let finalData = {
+      token: localStorage.getItem("token"),
+      planet_names: [selection1, selection2, selection3, selection4],
+      vehicle_names: [
+        selectedOption1.slice(0, -1),
+        selectedOption2.slice(0, -1),
+        selectedOption3.slice(0, -1),
+        selectedOption4.slice(0, -1),
+      ],
+    };
+
     try {
       const url = "https://findfalcone.geektrust.com/find";
       const headers = {
         Accept: "application/json",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       };
 
-   
+      const response = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(finalData),
+      });
 
-    const response = await fetch(url, {
-      method: "POST",
-      headers: headers,
-      body: JSON.stringify(finalData)
-    });
-
-    responseData = await response.json();
-    
-  } catch (error) {
-    console.error(error);
-  }
-  localStorage.setItem("response",JSON.stringify(responseData))
-  history.push('/results')
-  
-  }
+      responseData = await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+    localStorage.setItem("response", JSON.stringify(responseData));
+    history.push("/results");
+  };
   return (
     <div>
-      
       <div className="container">
-        
         <div className="item">
           <div>
             {" "}
             <label>
               Selection 1:
               <select value={selection1} onChange={handleSelection1Change}>
-                <option value="" >
-                  Select an option
-                </option>
+                <option value="" disabled>Select an option </option>
                 {data
                   ?.filter(
                     (item) =>
@@ -255,9 +258,7 @@ export default function Form(props) {
               <label>
                 Selection 2:
                 <select value={selection2} onChange={handleSelection2Change}>
-                  <option value="" >
-                    Select an option
-                  </option>
+                  <option value="" disabled>Select an option</option>
                   {data
                     ?.filter(
                       (item) =>
@@ -299,106 +300,120 @@ export default function Form(props) {
         </div>
       </div>
       <div class="sepreater">
-      <div  className="container">
-        <div className="item">
-          <div>
-            {selection2 ? (
-              <label>
-                Selection 3:
-                <select value={selection3} onChange={handleSelection3Change}>
-                  <option value="" >
-                    Select an option
-                  </option>
-                  {data
-                    ?.filter(
-                      (item) =>
-                        ![selection1, selection2, selection4].includes(item)
-                    )
-                    .map((element) => (
-                      <option value={element}>{element}</option>
-                    ))}
-                </select>
-              </label>
-            ) : (
-              <></>
-            )}
+        <div className="container">
+          <div className="item">
+            <div>
+              {selection2 ? (
+                <label>
+                  Selection 3:
+                  <select value={selection3} onChange={handleSelection3Change}>
+                    <option value="" disabled>Select an option</option>
+                    {data
+                      ?.filter(
+                        (item) =>
+                          ![selection1, selection2, selection4].includes(item)
+                      )
+                      .map((element) => (
+                        <option value={element}>{element}</option>
+                      ))}
+                  </select>
+                </label>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div>
+              {selection3 &&
+                vehicle.map((item) => {
+                  return (
+                    <div>
+                      <label htmlFor={item.name}>
+                        {item.name},({vehicleQty.get(item.name)})
+                      </label>
+                      <input
+                        type="checkbox"
+                        value={item.name + "3"}
+                        checked={item.name + "3" == selectedOption3}
+                        disabled={
+                          !vehicleQty.get(item.name) ||
+                          item.max_distance < planetdis[selection3]
+                        }
+                        id={item.name}
+                        name="3"
+                        onChange={handleVehicleSelection}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-          <div>{selection3 &&
-            vehicle.map((item) => {
-              return (
-                <div>
-                  <label htmlFor={item.name}>
-                    {item.name},({vehicleQty.get(item.name)})
-                  </label>
-                  <input
-                    type="checkbox"
-                    value={item.name + "3"}
-                    checked={item.name + "3" == selectedOption3}
-                    disabled={
-                      !vehicleQty.get(item.name) ||
-                      item.max_distance < planetdis[selection3]
-                    }
-                    id={item.name}
-                    name="3"
-                    onChange={handleVehicleSelection}
-                  />
-                </div>
-              );
-            })}</div>
-          
-          
-        </div>
-        <div className="item">
-          <div>{selection3 ? (
-            <label>
-              Selection 4:
-              <select value={selection4} onChange={handleSelection4Change}>
-                <option value="" >
-                  Select an option
-                </option>
-                {data
-                  ?.filter(
-                    (item) =>
-                      ![selection1, selection2, selection3].includes(item)
-                  )
-                  .map((element) => (
-                    <option value={element}>{element}</option>
-                  ))}
-              </select>
-            </label>
-          ) : (
-            <></>
-          )}</div>
-          <div>{selection4 &&
-            vehicle.map((item) => {
-              return (
-                <div>
-                  <label htmlFor={item.name}>
-                    {item.name},({vehicleQty.get(item.name)})
-                  </label>
-                  <input
-                    type="checkbox"
-                    value={item.name + "4"}
-                    checked={item.name + "4" == selectedOption4}
-                    disabled={
-                      !vehicleQty.get(item.name) ||
-                      item.max_distance < planetdis[selection4]
-                    }
-                    id={item.name}
-                    name="4"
-                    onChange={handleVehicleSelection}
-                  />
-                </div>
-              );
-            })}</div>
+          <div className="item">
+            <div>
+              {selection3 ? (
+                <label>
+                  Selection 4:
+                  <select value={selection4} onChange={handleSelection4Change}>
+                    <option value="" disabled>Select an option</option>
+                    {data
+                      ?.filter(
+                        (item) =>
+                          ![selection1, selection2, selection3].includes(item)
+                      )
+                      .map((element) => (
+                        <option value={element}>{element}</option>
+                      ))}
+                  </select>
+                </label>
+              ) : (
+                <></>
+              )}
+            </div>
+            <div>
+              {selection4 &&
+                vehicle.map((item) => {
+                  return (
+                    <div>
+                      <label htmlFor={item.name}>
+                        {item.name},({vehicleQty.get(item.name)})
+                      </label>
+                      <input
+                        type="checkbox"
+                        value={item.name + "4"}
+                        checked={item.name + "4" == selectedOption4}
+                        disabled={
+                          !vehicleQty.get(item.name) ||
+                          item.max_distance < planetdis[selection4]
+                        }
+                        id={item.name}
+                        name="4"
+                        onChange={handleVehicleSelection}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
         </div>
-        
-        <button className={(selection1 && selection2 && selection3 && selection4 &&selectedOption1 && selectedOption2 && selectedOption3 && selectedOption4)?"find-button":"disabled-button"} onClick={sendPostRequest}>Find Falcon</button> 
-        
-      
+
+        <button
+          className={
+            selection1 &&
+            selection2 &&
+            selection3 &&
+            selection4 &&
+            selectedOption1 &&
+            selectedOption2 &&
+            selectedOption3 &&
+            selectedOption4
+              ? "find-button"
+              : "disabled-button"
+          }
+          onClick={sendPostRequest}
+        >
+          Find Falcon
+        </button>
       </div>
-      <Table time={time}/>
+      <Table time={time} />
     </div>
   );
 }
